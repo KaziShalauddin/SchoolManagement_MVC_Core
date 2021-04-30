aa2072df10f1b30a561cff16c259cb923648bb90
KaziShalauddin <shalauddin99@gmail.com>
2021-04-30 18:25:59 +0600

    added student.cs and studentDbContext.cs

---
+++ SchoolManagement/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Models
+{
+    // [Table("Students")]
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Column(TypeName = "nvarchar(250)")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Reg. No.")]
+        public string RegistrationNo { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Roll { get; set; }
+    }
+}
