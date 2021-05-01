f4e7666ba1fca7c93f973a803de26fe8cb29bd97
KaziShalauddin <shalauddin99@gmail.com>
2021-05-01 12:05:58 +0600

    added migration and updated database

---
+++ SchoolManagement/Data/StudentDbMigrations/20210501054130_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolManagement.Data.StudentDbMigrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    RegistrationNo = table.Column<string>(type: "varchar(10)", nullable: false),
+                    Roll = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
